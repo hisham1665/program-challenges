67a7aa65f1edcc1eec60ad598e1836fc5542e830
Hisham K H <hishamkhofficial@gmail.com>
2025-07-14 08:04:51 +0530

    Create palindrome.py

---
+++ palindrome.py
@@ -0,0 +1,13 @@
+def checkPalindrome(num):
+    left = 0
+    right = len(num)-1
+    while (left < right):
+        if(num[left] != num[right]):
+            return False
+        left+=1
+        right-=1
+    return True
+
+num = input("Enter the number")
+result = checkPalindrome(num)
+print("Result : "+ str(result))
