c90ed268bef87b5228f93ea1662c057e53564131
Hisham K H <hishamkhofficial@gmail.com>
2025-07-15 23:27:53 +0530

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,3 +1,4 @@
+#this is a program to check the given number is pallindrome or not
 def checkPalindrome(num):
     left = 0
     right = len(num)-1
@@ -7,7 +8,6 @@ def checkPalindrome(num):
         left+=1
         right-=1
     return True
-
 num = input("Enter the number")
 result = checkPalindrome(num)
 print("Result : "+ str(result))
