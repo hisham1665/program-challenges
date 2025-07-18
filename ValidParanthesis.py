4386ad4941c8c1d36eb965430a64c6ec4514e251
Hisham K H <hishamkhofficial@gmail.com>
2025-07-18 08:40:05 +0530

    Create ValidParanthesis.py

---
+++ ValidParanthesis.py
@@ -0,0 +1,16 @@
+s = input("enter the srring")
+stack=[]
+for i in range(len(s)):
+    ch = s[i]
+    if(ch == "(" or ch == "[" or ch=="{"):
+        stack.append(ch)
+    elif(len(stack) != 0 and ch == ")" and "(" in stack):
+        stack.pop(stack.index("("))
+    elif(len(stack) != 0 and ch == "]" and "[" in stack):
+        stack.pop(stack.index("["))
+    elif(len(stack) != 0 and ch == "}" and "{" in stack):
+        stack.pop(stack.index("{"))
+    else:
+        print("false")
+print("true")
+        
