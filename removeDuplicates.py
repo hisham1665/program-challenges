6b678c207c76d1263a679d2f7c7ae9449185f474
Hisham K H <hishamkhofficial@gmail.com>
2025-07-23 22:52:58 +0530

    Create removeDuplicates.py

---
+++ removeDuplicates.py
@@ -0,0 +1,17 @@
+#this is the leet code problem number 83
+# remove duplicates form sorted list
+# this is the leetcode solution below
+
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution(object):
+    def deleteDuplicates(self, head):
+        current = head
+        while (current and current.next):
+            if (current.val == current.next.val):
+                current.next = current.next.next
+            else:
+                current = current.next
+        return head
