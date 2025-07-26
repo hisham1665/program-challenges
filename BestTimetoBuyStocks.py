03554fd1d775ae324e221b2be010149384e0ae61
Hisham K H <hishamkhofficial@gmail.com>
2025-07-26 22:11:50 +0530

    Update BestTimetoBuyStocks.py

--- BestTimetoBuyStocks.py
+++ BestTimetoBuyStocks.py
@@ -1,3 +1,5 @@
+# this is a program to solve the leet code problem of best time to buy and sell stocks
+#leetcode problem number 121
 prices = [7 , 1  , 5 , 3 , 6 , 4]
 max_profit = 0
 minimum = prices[0]
