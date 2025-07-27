7a139c26216c671f6dfef7a98a4c9b71fcee5d3c
Hisham K H <hishamkhofficial@gmail.com>
2025-07-27 23:37:35 +0530

    Create PlusOne.py

---
+++ PlusOne.py
@@ -0,0 +1,13 @@
+
+def plusOne( digits):
+        n = len(digits)
+        for i in range(n-1 , -1 ,-1):
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+            digits[i] = 0
+        res = [1] + digits
+        return res 
+st = input("Enter the digits")
+digits = [int(i) for i in st.split()]
+print(plusOne(digits))
