e4939e0deb0dd7ae3837529c7ab87b55e6be6642
Hisham K H <hishamkhofficial@gmail.com>
2025-08-05 22:32:19 +0530

    Create PalindromeLinkedList.py

---
+++ PalindromeLinkedList.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def isPalindrome(self, head):
+        if head == None:
+            return True
+        li = []
+        current = head
+        while current != None:
+            li.append(current.val)
+            current = current.next
+        left = 0
+        right = len(li) - 1
+        while right > left:
+            if(li[right] != li[left]):
+                return False
+            right -= 1
+            left += 1 
+        return True
