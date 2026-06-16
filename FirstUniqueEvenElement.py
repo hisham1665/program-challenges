af9409f153c7c9fb23e931c8929eaf424308bbd7
Hisham K H <hishamkhofficial@gmail.com>
2026-06-16 15:59:39 +0530

    Create FirstUniqueEvenElement.py

---
+++ FirstUniqueEvenElement.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def firstUniqueEven(self, nums):
+        hmap = {}
+        for i , dig in enumerate(nums):
+            if dig % 2 == 0:
+                if dig in hmap:
+                    hmap[dig] = -1
+                else :
+                    hmap[dig] = i
+        val = [v for v in hmap.values() if v >= 0 ]
+        return nums[min(val)] if val else -1
