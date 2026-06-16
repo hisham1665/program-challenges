43182309cdfa59a06889932ae80d73654575baf8
Hisham K H <hishamkhofficial@gmail.com>
2026-06-16 16:38:58 +0530

    Create FirstLetterToAppearTwics.py

---
+++ FirstLetterToAppearTwics.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def repeatedCharacter(self, s):
+        hmap = {}
+        for i , char in enumerate(s):
+            if char in hmap:
+                hmap[char] = hmap[char] + 1
+                if hmap[char] == 2:
+                    return char
+            else:
+                hmap[char] = 1
