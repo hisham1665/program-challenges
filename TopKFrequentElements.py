66723d825401140eff9578e59bef44b105cd2f82
Hisham K H <hishamkhofficial@gmail.com>
2026-06-17 15:38:30 +0530

    Create TopKFrequentElements.py

---
+++ TopKFrequentElements.py
@@ -0,0 +1,13 @@
+def topKFrequent(self, nums, k):
+        hmap={}
+        for i in nums :
+            if i in hmap :
+                hmap[i] += 1
+            else :
+                hmap[i] = 1
+        rt = []
+        for i in range(0 , k):
+            maxv = max(hmap , key=hmap.get)
+            rt.append(maxv) 
+            hmap[maxv] = -1
+        return rt
