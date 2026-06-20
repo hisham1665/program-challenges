d16128e5d4970015cde3c6fe1591d4f741d76042
Hisham K H <hishamkhofficial@gmail.com>
2026-06-20 22:04:49 +0530

    Create MaximumAverageSubarray1.py

---
+++ MaximumAverageSubarray1.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def findMaxAverage(self, nums, k):
+        current_sum = sum(nums[:k])
+        max_sum = current_sum
+        for i in range(k, len(nums)):
+            current_sum += nums[i] - nums[i - k]
+            if current_sum > max_sum:
+                max_sum = current_sum
+        return max_sum / float(k)
