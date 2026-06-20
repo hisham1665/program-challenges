df57e893cb88c9862e01c8f9941a97d8bd8a3fa7
Hisham K H <hishamkhofficial@gmail.com>
2026-06-20 16:53:03 +0530

    Create LongestSubstringWithoutRepetion.py

---
+++ LongestSubstringWithoutRepetion.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        hmap = set()
+        right = -1
+        left = maxsize = 0
+        for i in s :
+            if i in hmap :
+                if s[left] == i:
+                    left += 1
+                else :
+                    while s[left] != i:
+                        hmap.discard(s[left])
+                        left+= 1
+                    left += 1
+                right += 1
+            else : 
+                right += 1
+                if (right - left) + 1 > maxsize :
+                    maxsize = (right - left) + 1
+                hmap.add(i)
+        return maxsize        
