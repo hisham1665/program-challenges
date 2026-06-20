24286cf3e2d3ce8beba56a7ea5b63d1eb7729516
Hisham K H <hishamkhofficial@gmail.com>
2026-06-20 22:30:23 +0530

    Create MaximumNUmberofVowelsInaSubstringof Length.py

---
+++ MaximumNUmberofVowelsInaSubstringof Length.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def maxVowels(self, s, k):
+        left = 0
+        vow = {'a' , 'e' , 'i' ,'o' , 'u'}
+        right = k
+        count = 0
+        for i in s[left:right]:
+            if i in vow :
+                count += 1
+        maxc = count
+        for right in range(k,len(s)):
+            if s[left] in vow :
+                count -= 1
+            left += 1
+            if s[right] in vow :
+                count += 1
+            if count > maxc :
+                maxc = count
+        return maxc
+
+        
