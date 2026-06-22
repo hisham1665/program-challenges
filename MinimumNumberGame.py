15d8e3de0763004a6310a29693d80637a2da4b9a
Hisham K H <hishamkhofficial@gmail.com>
2026-06-22 18:18:50 +0530

    Update MinimumNumberGame.py

--- MinimumNumberGame.py
+++ MinimumNumberGame.py
@@ -10,3 +10,16 @@ class Solution(object):
             out.append(nums[k])
         return out
         
+
+# another solution
+class Solution(object):
+    def numberGame(self, nums):
+        nums = sorted(nums)
+        k = j = -1
+        while k < len(nums) -2:
+            k = j + 1
+            j = k+1
+            nums[k] = nums[k] + nums[j]
+            nums[j] = nums[k] - nums[j]
+            nums[k] = nums[k] - nums[j]
+        return nums
