5fec86d46ff22d3dab31fc62cd1c3baa9a988ffb
Hisham K H <hishamkhofficial@gmail.com>
2026-07-09 20:07:15 +0530

    Create ContainerWithMostWater.py

---
+++ ContainerWithMostWater.py
@@ -0,0 +1,17 @@
+ class Solution(object):
+    def maxArea(self, height):
+        left = 0 
+        right = len(height) - 1
+        hi , max = 0 , 0
+        while left < right :
+            if height[right] > height[left]:
+                hi = height[left] * (right - left)
+                left+= 1
+            else :
+                hi = height[right] * (right - left)
+                right -= 1
+            if max < hi:
+                max = hi
+        return max
+
+        
