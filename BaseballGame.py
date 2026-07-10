b85659e99a74d31edaae5fb2607264d4444d517c
Hisham K H <hishamkhofficial@gmail.com>
2026-07-10 20:16:38 +0530

    Implement baseball game score calculation

---
+++ BaseballGame.py
@@ -0,0 +1,15 @@
+class Solution:
+    def calPoints(self, operations: List[str]) -> int:
+        record = []
+        for  ch in operations:
+            if ch == '+' :
+                su = record[-1] + record[-2] 
+                record.append(su)
+            elif  ch == 'D' :
+                pro = record[-1] * 2
+                record.append(pro)
+            elif ch =='C' :
+                record.pop()
+            else :
+                record.append(int(ch))
+        return sum(record)
