cc12b31169bc3af76adfe18be48311da62323619
Hisham K H <hishamkhofficial@gmail.com>
2026-07-13 22:31:21 +0530

    Update ValidParanthesis.py

--- ValidParanthesis.py
+++ ValidParanthesis.py
@@ -1,18 +1,22 @@
 #this is a program to find the valid parathesis problem in leetcode
 #leetcode problem 20
-s = input("enter the srring")
-stack=[]
-for i in range(len(s)):
-    ch = s[i]
-    if(ch == "(" or ch == "[" or ch=="{"):
-        stack.append(ch)
-    elif(len(stack) != 0 and ch == ")" and "(" in stack):
-        stack.pop(stack.index("("))
-    elif(len(stack) != 0 and ch == "]" and "[" in stack):
-        stack.pop(stack.index("["))
-    elif(len(stack) != 0 and ch == "}" and "{" in stack):
-        stack.pop(stack.index("{"))
-    else:
-        print("false")
-print("true")
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack=[]
+        for i in range(len(s)):
+            ch = s[i]
+            if(ch == "(" or ch == "[" or ch=="{"):
+                stack.append(ch)
+            else :
+                if not stack :
+                    return False
+                elif ch == ")" and stack[-1] != "(" :
+                    return False
+                elif ch == "]" and stack[-1] != "[":
+                    return False
+                elif ch == "}" and stack[-1] != "{":
+                    return False
+                else :
+                    stack.pop()
+        return len(stack) == 0
         
