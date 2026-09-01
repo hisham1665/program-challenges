eec6828fa695528ab898f4be8584bd0e6107d9f5
Hisham K H <hishamkhofficial@gmail.com>
2026-09-01 20:46:39 +0530

    Create house-robber.py

---
+++ house-robber.py
@@ -0,0 +1,7 @@
+def rob(self, nums):
+        n = len(nums)
+        dp = [0] * n
+        dp[0] = nums[0]
+        for i in range(1, n) :
+            dp[i] = max(dp[i-2] + nums[i] , dp[i-1])
+        return dp[n-1]
